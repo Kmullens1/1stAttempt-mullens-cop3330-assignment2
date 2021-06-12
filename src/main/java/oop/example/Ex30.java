e6a744c959a2821a643559e7ca90855e2d719c66
kmullens1 <kmullens1@knights.ucf.edu>
2021-06-12 01:56:14 -0400

    First Commit

---
+++ src/main/java/oop/example/Ex30.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Keri Mullens
+ */
+package oop.example;
+
+public class Ex30 {
+    public static void main(String[] args) {
+        //Multiplication Table
+        /*Create a program that generates multiplication tables for the numbers 0 through 12.
+        Example Output
+            0 X 0 = 0
+            0 X 1 = 0
+            ...
+            12 x 11 = 132
+            12 x 12 = 144
+        Constraint
+            Use a nested loop to complete this program.*/
+
+    }
+}
