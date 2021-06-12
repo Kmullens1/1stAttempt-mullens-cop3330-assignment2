3f01f259d82a2a6d7b93357ecb6fe80bbef85ae7
kmullens1 <kmullens1@knights.ucf.edu>
2021-06-12 01:52:15 -0400

    First Commit

---
+++ src/main/java/oop/example/Ex24.java
@@ -0,0 +1,39 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Keri Mullens
+ */
+package oop.example;
+
+import java.util.Scanner;
+
+public class Ex24 {
+    public static void main(String[] args) {
+        //Anagram Checker
+        /*Using functions to abstract the logic away from the rest of your code makes it easier to read
+        and easier to maintain.
+        Create a program that compares two strings and determines if the two strings are anagrams. The
+        program should prompt for both input strings and display the output as shown in the example that
+        follows.
+        Example Output
+            Enter two strings and I'll tell you if they are anagrams:
+            Enter the first string: note
+            Enter the second string: tone
+            "note" and "tone" are anagrams.
+        Constraints
+            Implement the program using a function called isAnagram, which takes in two words as its
+            arguments and returns true or false. Invoke this function from your main program.
+            Check that both words are the same length.*/
+        System.out.print("Enter two strings and I'll tell you if they are anagrams:\nEnter the first string:");
+        Scanner input = new Scanner(System.in);
+        String first = input.nextLine();
+
+        System.out.print("Enter the second string:");
+        String second = input.nextLine();
+
+        //Create a function to compare the strings
+
+        System.out.println(first + " and " + second + " are anagrams.");
+        System.out.println(first + " and " + second + " are not anagrams.");
+
+    } //Don't forget to add in Junit 5 and test cases
+}
